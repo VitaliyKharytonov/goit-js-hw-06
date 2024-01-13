76f552aec89214258a35004547e571532d7a24a8
VitaliyKharytonov <vitalicaxaritonov@gmail.com>
2024-01-13 23:49:18 +0200

    add 2 task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = ingredients.map((name) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = name;
+  ingredient.classList = "item";
+  //ingredient.classList.add("item");
+  return ingredient;
+});
+
+const listEL = document.querySelector("#ingredients");
+listEL.append(...listIngredients);
+
+// const listIngredients = ingredients
+//   .map((name) => `<li class='item'> ${name} </li>`)
+//   .join("");
+// listEL.innerHTML = listIngredients;
