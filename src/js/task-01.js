11520743415304e97a08219fe6f722487e94ed92
VitaliyKharytonov <vitalicaxaritonov@gmail.com>
2024-02-18 14:29:01 +0200

    add start file

--- src/js/task-01.js
+++
@@ -1,8 +0,0 @@
-const categoriesEL = document.querySelectorAll(".item");
-
-console.log(`Number of categories: ${categoriesEL.length}`);
-
-categoriesEL.forEach((category) => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
-  console.log(`Elements: ${category.lastElementChild.children.length}`);
-});
